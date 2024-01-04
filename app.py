305cc6943067b0939cc96ef3e19b85482d2d55d5
Binh Thanh Le <49960555+binhthanhle@users.noreply.github.com>
2024-01-04 23:04:20 +0000

    push

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def matching(path1 = None, path2 = None, kind:str = 'path', draw:bool =False) ->
     """
     img1, img2 = get_input(kind, path1, path2)
     fig, ax = plt.subplots(figsize=(5, 5))
-    plt.imshow(img_match, 'gray', ax=ax),
+    plt.imshow(img1, 'gray', ax=ax),
     st.pyplot(fig)
     # Size:
     # h,w,s = img1.shape
